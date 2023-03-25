2862c5841065e3e2b780b7213c216f78a1814db6
Dalton Daly <dalton.daly@yahoo.com>
2023-03-25 09:56:04 -0500

    Kata

---
+++ Fundamentals/ReverseWords.js
@@ -0,0 +1,27 @@
+let str = "This is a string of words";
+
+function reverseWords(str) {
+  let newStr = [];
+  let letters = str.split("");
+  let words = str.split(" ");
+  let spaceIndexes = [];
+  
+  letters.forEach((letter, index) => {
+    if(letter === " "){
+      spaceIndexes.push(index);
+    }
+  })
+  
+  for (let i = 0; i < words.length; i++){
+    let reverseWord = "";
+    for (let j = words[i].length - 1; j >= 0; j--) {
+      reverseWord += words[i][j];
+    }
+    newStr.push(reverseWord);
+  }
+  
+  newStr = newStr.join(" ");
+  return newStr;
+}
+
+console.log(reverseWords(str));
